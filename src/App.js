c32e21081004c470b06cf1fd1b4b43edebcea2b2
pabhi-pete <pabhi@example.com>
2021-12-28 15:50:11 -0800

    crated Dockerfile.dev

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome! to first Docker frontend deployment
         </p>
         <a
           className="App-link"
