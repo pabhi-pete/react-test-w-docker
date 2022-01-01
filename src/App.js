934caf57471d9c791fd75201198ddd6042c9c238
pabhi-pete <pabhi@example.com>
2021-12-31 19:14:41 -0800

    successful running app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome! to first Docker frontend deployment
+          this is awesome OMG
+          
         </p>
         <a
           className="App-link"
